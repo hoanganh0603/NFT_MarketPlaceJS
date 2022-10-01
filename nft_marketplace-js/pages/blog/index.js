acb5ee0bb1ca6e7a4d05cd699cec7d80fd3a3f16
hoanganh0603 <nguyengiahoanganh@gmail.com>
2022-10-01 21:19:19 +0700

    First Commit

---
+++ nft_marketplace-js/pages/blog/index.js
@@ -0,0 +1,24 @@
+/** jsx?|tsx? file header */
+// import Link from "next/link";
+// import { MainLayout } from "@ui/layout";
+
+// export default function BlogPage() {
+//   return (
+//     <>
+//       <div>Blog Page</div>
+//       {/* <Link href="/">
+//         <a>Go to Home Page</a>
+//       </Link> */}
+//     </>
+//   );
+// }
+
+// BlogPage.Layout = MainLayout;
+
+export default function BlogPage() {
+  return (
+    <>
+      <div>Blog Page</div>
+    </>
+  );
+}
